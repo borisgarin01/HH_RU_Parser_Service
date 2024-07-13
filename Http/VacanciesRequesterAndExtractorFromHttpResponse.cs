bfe89ac7942643fe564952a2fae55e3eb8dcdde1
boris.garin01 <boris.garin01job@mail.ru>
2024-07-13 14:48:39 +0300

    VacanciesRequesterAndExtractorFromHttpResponse moved into separate file

---
+++ Http/VacanciesRequesterAndExtractorFromHttpResponse.cs
@@ -0,0 +1,51 @@
+﻿using HH_RU_ParserService.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HH_RU_ParserService.Http
+{
+    public sealed class VacanciesRequesterAndExtractorFromHttpResponse
+    {
+        HttpClient httpClient;
+        public VacanciesRequesterAndExtractorFromHttpResponse()
+        {
+            httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+        }
+        public async Task<Root> GetVacanciesResponseObjectViaHttpQuery(string text, string employment = "full", string schedule = "remote", string experience = "noExperience", int? areaId = null, string[] searchFields = null, int page = 0)
+        {
+            HttpResponseMessage responseMessage = null;
+            if (searchFields is null)
+            {
+                if (areaId is null)
+                {
+                    responseMessage = await httpClient.GetAsync($"https://api.hh.ru/vacancies?text={text}&employment={employment}&schedule={schedule}&page={page}&experience={experience}");
+                }
+                else
+                {
+                    responseMessage = await httpClient.GetAsync($"https://api.hh.ru/vacancies?text={text}&employment={employment}&schedule={schedule}&page={page}&experience={experience}&areaId={areaId}");
+                }
+            }
+            else
+            {
+                if (areaId is null)
+                {
+                    responseMessage = await httpClient.GetAsync($"https://api.hh.ru/vacancies?text={text}&employment={employment}&schedule={schedule}&page={page}&experience={experience}&{string.Join('&', searchFields.Select(sf => $"search_field={sf}"))}");
+                }
+                else
+                {
+                    responseMessage = await httpClient.GetAsync($"https://api.hh.ru/vacancies?text={text}&employment={employment}&schedule={schedule}&page={page}&experience={experience}&{string.Join('&', searchFields.Select(sf => $"search_field={sf}"))}&areaId={areaId}");
+                }
+            }
+            Root root = null;
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                root = await JsonSerializer.DeserializeAsync<Root>(await responseMessage.Content.ReadAsStreamAsync());
+            }
+
+            return root;
+        }
+    }
+}
